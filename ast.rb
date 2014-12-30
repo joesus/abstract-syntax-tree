6209628749e4c1db6e85b19d3f2221a3565e5aec
Joe Susnick <joesusnick@gmail.com>
2014-12-30 10:18:35 -0700

    initial commit

---
+++ ast.rb
@@ -0,0 +1,9 @@
+require 'pry-nav'
+
+class AST
+
+  def initialize
+    # figure out what I'm building... and build it.
+  end
+
+end
---
+++ ast_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require_relative 'hash'
+
+class ASTTest < MiniTest::Unit::TestCase
+
+  def setup
+    # initialize an ast
+  end
+
+  def test_to_s
+    # print out the ast
+  end
+
+end

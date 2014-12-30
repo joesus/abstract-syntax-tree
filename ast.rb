659ffcedb7c2e9b772247d180b80c102c218ae7c
Joe Susnick <joesusnick@gmail.com>
2014-12-30 13:43:57 -0700

    to_s method working with recursion

--- ast.rb
+++ ast.rb
@@ -10,11 +10,22 @@ class ASTNode
   end
 
   def to_s
-    # implement to_s
+    string ||= ""
+    if self.left.leaf?
+      string << "#{self.left.value}"
+    else
+      string << "(#{self.left.to_s})"
+    end
+    string << " #{self.value} "
+    if self.right.leaf?
+      string << "#{self.right.value}"
+    else
+      string << "(#{self.right.to_s})"
+    end
   end
 
   def leaf?
-    self.left.nil? && self.right.nil?
+    self.left.nil? && self.right.nil? rescue nil
   end
 
 end
--- ast_test.rb
+++ ast_test.rb
@@ -7,7 +7,9 @@ class ASTTest < MiniTest::Unit::TestCase
     @default_ast = ASTNode.new(:+, ASTNode.new(1), ASTNode.new(2))
     @zero_branches_ast = ASTNode.new(:+)
     @one_branch_ast = ASTNode.new(:+, ASTNode.new(1))
-    @two_level_ast = ASTNode.new(:+, ASTNode.new(:+, ASTNode.new(1), ASTNode.new(2)), ASTNode.new(3))
+    @two_levels_left = ASTNode.new(:+, ASTNode.new(:+, ASTNode.new(1), ASTNode.new(2)), ASTNode.new(3))
+    @two_levels_right = ASTNode.new(:+, ASTNode.new(1), ASTNode.new(:+, ASTNode.new(2), ASTNode.new(3)))
+    @two_levels_both = ASTNode.new(:+, ASTNode.new(:+, ASTNode.new(1), ASTNode.new(2)), ASTNode.new(:+, ASTNode.new(3), ASTNode.new(4)))
   end
 
   def test_ast_initializes_with_correct_values
@@ -28,6 +30,16 @@ class ASTTest < MiniTest::Unit::TestCase
 
   def test_leaf?
     assert_equal true, @default_ast.left.leaf?
-    assert_equal false, @two_level_ast.left.leaf?
+    assert_equal false, @two_levels_left.left.leaf?
+  end
+
+  def test_to_s_one_level
+    assert_equal "1 + 2", @default_ast.to_s
+  end
+
+  def test_to_s_two_levels
+    assert_equal "(1 + 2) + 3", @two_levels_left.to_s
+    assert_equal "1 + (2 + 3)", @two_levels_right.to_s
+    assert_equal "(1 + 2) + (3 + 4)", @two_levels_both.to_s
   end
 end
